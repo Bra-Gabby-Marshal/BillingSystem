87b9f659f269e20232a1956537045ec870247a0d
Sodey Haidor Gabriel <sodeygabriel123@gmail.com>
2022-10-24 16:06:54 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='GabbyMarshalBillingSystem',
+    version='2.5',
+    packages=[''],
+    url='www.billing.com',
+    license='github',
+    author='Sodey Haidor Gabriel',
+    author_email='sodeygabriel123@gmail.com',
+    description='Do all your billing stuffs with ease'
+)
